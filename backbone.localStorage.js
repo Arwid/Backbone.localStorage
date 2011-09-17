932540e0fcf6323c0e850416a0e5ba915c480ff6
Arwid Bancewicz <arwid@arwid.ca>
2011-09-17 19:08:26 -0400

    Adding project files

--- backbone.localStorage.js
+++ backbone.localStorage.js
@@ -1,185 +1,129 @@
-// A simple module to replace `Backbone.sync` with *localStorage*-based
-// persistence. Models are given GUIDS, and saved into a JSON object. Simple
-// as that.
-// Generate four random hex digits.
-function S4() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-};
-
-// Generate a pseudo-GUID by concatenating random hexadecimal.
-function guid() {
-    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-};
-
-// Our Store is represented by a single JS object in *localStorage*. Create it
-// with a meaningful name, like the name you'd give a table.
-window.Store = function(name) {
-    this.name = name;
-    var store = localStorage.getItem(this.name);
-    this.records = (store && store.split(",")) || [];
-};
-
-_.extend(Store.prototype, {
-
-    // Save the current state of the **Store** to *localStorage*.
-    save: function() {
-        localStorage.setItem(this.name, this.records.join(","));
-    },
-
-    // Add a model, giving it a (hopefully)-unique GUID, if it doesn't already
-    // have an id of it's own.
-    create: function(model) {
-        if (!model.id) model.set({id: guid()});
-        localStorage.setItem(this.name + "-" + model.id, JSON.stringify(model));
-        this.records.push(model.id.toString());
-        this.save();
-        return model;
-    },
-
-    // Update a model by replacing its copy in `this.data`.
-    update: function(model) {
-        localStorage.setItem(this.name + "-" + model.id, JSON.stringify(model));
-        if (!_.include(this.records, model.id.toString())) this.records.push(model.id.toString());
-        this.save();
-        return model;
-    },
-
-    // Retrieve a model from `this.data` by id.
-    find: function(model) {
-        return JSON.parse(localStorage.getItem(this.name + "-" + model.id));
-    },
-
-    // Return the array of all models currently in storage.
-    findAll: function() {
-        return _.map(this.records,
-        function(id) {
-            return JSON.parse(localStorage.getItem(this.name + "-" + id))
-        },
-        this);
-    },
-
-    // Delete a model from `this.data`, returning it.
-    destroy: function(model) {
-        localStorage.removeItem(this.name + "-" + model.id);
-        this.records = _.reject(this.records,
-        function(record_id) {
-            return record_id == model.id.toString();
-        });
-        this.save();
-        return model;
-    },
-
-    // Clear **localStorage**
-    clear: function() {
-        //localStorage.clear(); //removes all storage
-        localStorage.removeItem(this.name);
-        _.each(this.records,$.proxy(function(todo) {
-          localStorage.removeItem(this.name + "-" + todo.id);
-        },this));
-        this.records = [];
-    },
-
-    // Retrieve from the database into *localStorage*.
-    loadFromDB: function(url, success, error) {
-        Backbone.sync = Backbone.syncLocal;
-        // setup response handlers
-        var lfSuccess = $.proxy(function(records, asuccess, response){
-          this.clear();
-          var store = _.pluck(records, "id").join(",");
-          localStorage.setItem(this.name, store);
-          $(records).each($.proxy(function(index, item) {
-              localStorage.setItem(this.name + "-" + item.id, JSON.stringify(item));
-          },this));
-          this.records = (store && store.split(",")) || [];
-          success && success(records, asuccess, response);
-        },this);
-        var lfError = $.proxy(function(response, error, message) {
-            error && error(response, error, message);
-        },
-        this);
-        // Compile JSON-request options.
-        var params = {
-            url: url,
-            type: 'GET',
-            contentType: 'application/json',
-            dataType: 'json',
-            processData: false,
-            success: lfSuccess,
-            error: lfError
-        };
-        // Make the request.
-        $.ajax(params);
-    },
-    
-    // Saves all of it to the server
-    saveToDB: function(url, success, error) {
-      
-      var records = this.findAll();
-      var data = JSON.stringify(records);
-      
-      // setup response handlers
-      var lfSuccess = $.proxy(function(records, asuccess, response){
-        this.clear();
-        // update todos to use new server provided ids
-        $(records).each($.proxy(function(index, item){
-          var todo = Todos.get(item.from_id);
-          todo.set({
-            id: item.id
-          });
-          this.update(todo);
-          },this));
-      },this);
-      var lfError = $.proxy(function(response, error, message) {
-        error && error(response, error, message);
-      },
-      this);
-      
-      // Compile JSON-request options.
-      var params = {
-          url: url,
-          type: 'PUT',
-          contentType: 'application/json',
-          data: data,
-          dataType: 'json',
-          processData: false,
-          success: lfSuccess,
-          error: lfError
-      };
-      // Make the request.
-      $.ajax(params);
+(function() {
+  /*!
+  Backbone localStorage Adapter v1.0
+  https://github.com/Arwid/Backbone.localStorage/tree/coffeescript
+  */
+  /*
+  LocalStore adds *localStorage*-based persistence to a Backbone collection
+  Dependencies: Underscore.js
+  * Backbone.js is not a hard dependency
+  */
+  var _CRUD_Sync, _S4, _guid, _guid_couch;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  window.LocalStore = (function() {
+    var modelIdAttribute;
+    modelIdAttribute = "id";
+    function LocalStore(collection, name) {
+      var store;
+      this.collection = collection;
+      this.name = name;
+      this._save = __bind(this._save, this);
+      this.create = __bind(this.create, this);
+      this.collection.sync = _CRUD_Sync;
+      this.collection.model.prototype.sync = _CRUD_Sync;
+      this.collection.store = this;
+      modelIdAttribute = this.collection.model.prototype.idAttribute;
+      store = localStorage.getItem(this.name);
+      this.records = (store && store.split(",")) || [];
     }
-
-});
-
-// Override `Backbone.sync` to use delegate to the model or collection's
-// *localStorage* property, which should be an instance of `Store`.
-Backbone.syncLocal = function(method, model, success, error) {
-
-    var resp;
-    var store = model.localStorage || model.collection.localStorage;
-
+    /* 
+    PUBLIC CRUD Methods
+    */
+    LocalStore.prototype.create = function(model) {
+      var attributes;
+      attributes = {};
+      if (!model.id) {
+        attributes[modelIdAttribute] = _guid();
+      }
+      model.set(attributes);
+      return this._save(model);
+    };
+    LocalStore.prototype.find = function(model) {
+      return JSON.parse(localStorage.getItem(this.name + "-" + model.id));
+    };
+    LocalStore.prototype.findAll = function() {
+      return _.map(this.records, (function(id) {
+        return JSON.parse(localStorage.getItem(this.name + "-" + id));
+      }), this);
+    };
+    LocalStore.prototype.update = function(model) {
+      return this._save(model);
+    };
+    LocalStore.prototype.destroy = function(model) {
+      localStorage.removeItem(this.name + "-" + model.id);
+      this.records = _.reject(this.records, function(record_id) {
+        return record_id === model.id.toString();
+      });
+      this._save();
+      return model;
+    };
+    /*
+      EXTRA Methods
+      */
+    LocalStore.prototype.clear = function() {
+      localStorage.removeItem(this.name);
+      _.each(this.records, $.proxy(function(todo) {
+        return localStorage.removeItem(this.name + "-" + todo.id);
+      }, this));
+      return this.records = [];
+    };
+    /*
+      PRIVATE Methods (by "_" convention)
+      */
+    LocalStore.prototype._save = function(model) {
+      if (model == null) {
+        model = void 0;
+      }
+      if (model) {
+        this._saveModel(model);
+      }
+      localStorage.setItem(this.name, this.records.join(","));
+      return model;
+    };
+    LocalStore.prototype._saveModel = function(model) {
+      localStorage.setItem(this.name + "-" + model.id, JSON.stringify(model));
+      if (!_.include(this.records, model.id.toString())) {
+        return this.records.push(model.id.toString());
+      }
+    };
+    return LocalStore;
+  })();
+  _CRUD_Sync = function(method, model, options, error) {
+    var resp, store;
+    if (typeof options === 'function') {
+      options = {
+        success: options,
+        error: error
+      };
+    }
+    store = model.store || model.collection.store;
     switch (method) {
-    case "read":
-        resp = model.id ? store.find(model) : store.findAll();
-        break;
-    case "create":
+      case "create":
         resp = store.create(model);
         break;
-    case "update":
+      case "read":
+        resp = (model.id ? store.find(model) : store.findAll());
+        break;
+      case "update":
         resp = store.update(model);
         break;
-    case "delete":
+      case "delete":
         resp = store.destroy(model);
-        break;
     }
-
     if (resp) {
-        success(resp);
+      return options.success(resp);
     } else {
-        error("Record not found");
+      return options.error("Record not found");
     }
-};
-Backbone.origSync = Backbone.sync;
-
-// To use **localStorage**
-// Backbone.sync = Backbone.syncLocal;
+  };
+  _S4 = function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  _guid_couch = function() {
+    return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
+  };
+  _guid = function() {
+    return S4() + S4() + S4() + S4() + S4() + S4();
+  };
+  window.Store = LocalStore;
+}).call(this);
---
+++ backbone.localStorage.js.coffee
@@ -0,0 +1,131 @@
+###!
+Backbone localStorage Adapter v1.0
+https://github.com/Arwid/Backbone.localStorage/tree/coffeescript
+###
+
+###
+LocalStore adds *localStorage*-based persistence to a Backbone collection
+Dependencies: Underscore.js
+* Backbone.js is not a hard dependency
+###
+class window.LocalStore
+  
+  modelIdAttribute = "id" # for easy referencing
+  
+  constructor: (@collection, @name) ->
+    
+    # Setup Sync
+    @collection.sync = _CRUD_Sync
+    @collection.model::sync = _CRUD_Sync
+    @collection.store = this # bidirectional
+    
+    # Get ID Attribute for model creation
+    modelIdAttribute = @collection.model::idAttribute
+    
+    # Get Records from *localStorage*
+    store = localStorage.getItem(@name)
+    @records = (store and store.split(",")) or []
+    
+  ### 
+  PUBLIC CRUD Methods
+  ###
+  
+  # CREATE (Add) model giving it a (hopefully)-unique GUID, if it doesn't have 
+  # one already
+  create: (model) =>
+    attributes = {}
+    attributes[modelIdAttribute] = _guid() unless model.id
+    
+    model.set attributes
+    @_save model
+  
+  # READ (Retrieve) a model by id
+  find: (model) ->
+    JSON.parse localStorage.getItem(@name + "-" + model.id)
+  
+  # READ (Retrieve) an array of all models currently in storage
+  findAll: ->
+    _.map @records, ((id) ->
+      JSON.parse localStorage.getItem(@name + "-" + id)
+    ), this
+  
+  # UPDATE model by replacing its copy in storage
+  update: (model) ->
+    @_save model
+  
+  # DELETE (Destroy) model from this store, returning it
+  destroy: (model) ->
+    localStorage.removeItem @name + "-" + model.id
+    @records = _.reject(@records, (record_id) ->
+      record_id == model.id.toString()
+    )
+    @_save()
+    model
+  
+  ###
+  EXTRA Methods
+  ###
+  
+  # Clear *localstorage*
+  clear: ->
+    localStorage.removeItem @name
+    _.each @records, $.proxy((todo) ->
+      localStorage.removeItem @name + "-" + todo.id
+    , this)
+    @records = []
+    
+  ###
+  PRIVATE Methods (by "_" convention)
+  ###
+  
+  # Save the current state of the **store** to *localstorage*, optionally
+  # saving a model
+  _save: (model = undefined) =>
+    @_saveModel model if model
+    localStorage.setItem @name, @records.join(",")
+    model
+
+  # Save a model to *localstorage*
+  _saveModel: (model) ->
+    localStorage.setItem @name + "-" + model.id, JSON.stringify(model)
+    unless _.include(@records, model.id.toString())
+      @records.push model.id.toString() 
+  
+# Sync method to use instead of Backbone.sync which delegates to the model or
+# collection's *store* property, which should be an instance of 'LocalStore''
+_CRUD_Sync = (method, model, options, error) ->
+  
+  # Backwards compatibility with Backbone <= 0.3.3
+  if typeof options == 'function'
+    options = success: options, error: error
+  
+  store = model.store or model.collection.store
+  switch method
+    when "create"
+      resp = store.create(model)
+    when "read"
+      resp = (if model.id then store.find(model) else store.findAll())
+    when "update"
+      resp = store.update(model)
+    when "delete"
+      resp = store.destroy(model)
+  if resp
+    options.success resp
+  else
+    options.error "Record not found"
+
+# Generate four random hex digits.
+_S4 = ->
+  (((1 + Math.random()) * 0x10000) | 0).toString(16).substring 1
+  
+# Generate a pseudo-GUID by concatenating random hexadecimal.  
+_guid_couch = ->
+  S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4()
+
+# Generate a pseudo-GUID using the BSON format for MongoDB.
+_guid = ->
+  S4() + S4() + S4() + S4() + S4() + S4()
+
+# _.extend window.LocalStore.prototype, Backbone.Events
+
+window.Store = LocalStore
